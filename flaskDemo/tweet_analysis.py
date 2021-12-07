27a851005705e7265afe9e75072b2268691c4041
Brian Nguyen <bp.nguyen05@gmail.com>
2021-12-07 14:00:34 -0600

    Fixed some formatting

--- flaskDemo/tweet_analysis.py
+++ flaskDemo/tweet_analysis.py
@@ -13,42 +13,41 @@ def create_twitter_url():
     tweet_fields = 'tweet.fields=id,created_at,public_metrics,possibly_sensitive'
     expansions = 'expansions=author_id'
     user_fields = 'user.fields=created_at,public_metrics'
-    #url = "https://api.twitter.com/2/tweets/20?{}&{}".format(tweet_fields, expansions)
-    url = "https://api.twitter.com/2/tweets?{}&{}&{}&{}".format(id, tweet_fields, expansions, user_fields)
+    url = 'https://api.twitter.com/2/tweets?{}&{}&{}&{}'.format(id, tweet_fields, expansions, user_fields)
     return url
 
 def process_yaml():
-    with open("config.yaml") as file:
+    with open('config.yaml') as file:
         return yaml.safe_load(file)
 
 def create_bearer_token(data):
-    return data["search_tweets_api"]["bearer_token"]
+    return data['search_tweets_api']['bearer_token']
 
 def twitter_auth_and_connect(bearer_token, url):
     headers = {
-        "Authorization": "Bearer {}".format(bearer_token),
+        'Authorization': 'Bearer {}'.format(bearer_token),
         'User-Agent': 'v2TweetLookupPython'
     }
-    response = requests.request("GET", url, headers=headers)
+    response = requests.request('GET', url, headers=headers)
     return response.json()
 
 def lang_data_shape(res_json):
-    data_only = res_json["data"]
-    doc_start = '"documents": {}'.format(data_only)
-    str_json = "{" + doc_start + "}"
+    data_only = res_json['data']
+    doc_start = '\'documents\': {}'.format(data_only)
+    str_json = '{' + doc_start + '}'
     dump_doc = json.dumps(str_json)
     doc = json.loads(dump_doc)
     return ast.literal_eval(doc)
 
 def connect_to_azure(data):
-    azure_url = "https://week.cognitiveservices.azure.com/"
-    language_api_url = "{}text/analytics/v2.1/languages".format(azure_url)
-    sentiment_url = "{}text/analytics/v2.1/sentiment".format(azure_url)
-    subscription_key = data["azure"]["subscription_key"]
+    azure_url = 'https://week.cognitiveservices.azure.com/'
+    language_api_url = '{}text/analytics/v2.1/languages'.format(azure_url)
+    sentiment_url = '{}text/analytics/v2.1/sentiment'.format(azure_url)
+    subscription_key = data['azure']['subscription_key']
     return language_api_url, sentiment_url, subscription_key
 
 def azure_header(subscription_key):
-    return {"Ocp-Apim-Subscription-Key": subscription_key}
+    return {'Ocp-Apim-Subscription-Key': subscription_key}
 
 def generate_languages(headers, language_api_url, documents):
     response = requests.post(language_api_url, headers=headers, json=documents)
@@ -56,20 +55,20 @@ def generate_languages(headers, language_api_url, documents):
 
 
 def combine_lang_data(documents, with_languages):
-    langs = pd.DataFrame(with_languages["documents"])
-    lang_iso = [x.get("iso6391Name")
+    langs = pd.DataFrame(with_languages['documents'])
+    lang_iso = [x.get('iso6391Name')
                 for d in langs.detectedLanguages if d for x in d]
-    data_only = documents["documents"]
+    data_only = documents['documents']
     tweet_data = pd.DataFrame(data_only)
-    tweet_data.insert(2, "language", lang_iso, True)
-    json_lines = tweet_data.to_json(orient="records")
+    tweet_data.insert(2, 'language', lang_iso, True)
+    json_lines = tweet_data.to_json(orient='records')
     return json_lines
 
 
 def add_document_format(json_lines):
-    docu_format = '"' + "documents" + '"'
-    json_docu_format = "{}:{}".format(docu_format, json_lines)
-    docu_align = "{" + json_docu_format + "}"
+    docu_format = ''' + 'documents' + '''
+    json_docu_format = '{}:{}'.format(docu_format, json_lines)
+    docu_align = '{' + json_docu_format + '}'
     jd_align = json.dumps(docu_align)
     jl_align = json.loads(jd_align)
     return ast.literal_eval(jl_align)
@@ -82,17 +81,17 @@ def sentiment_scores(headers, sentiment_url, document_format):
 
 
 def mean_score(sentiments):
-    sentiment_df = pd.DataFrame(sentiments["documents"])
-    return sentiment_df["score"].mean()
+    sentiment_df = pd.DataFrame(sentiments['documents'])
+    return sentiment_df['score'].mean()
 
 
 def week_logic(week_score):
     if week_score > 0.75 or week_score == 0.75:
-        print("You had a positve week")
+        print('You had a positve week')
     elif week_score > 0.45 or week_score == 0.45:
-        print("You had a neautral week")
+        print('You had a neautral week')
     else:
-        print("You had a negative week, I hope it gets better")
+        print('You had a negative week, I hope it gets better')
 
 ########## Metric Conversions ##########
 
@@ -208,5 +207,5 @@ def main():
 
     # Perhaps sort subset of matching songs by popularity and choose most popular songs for virability?
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
