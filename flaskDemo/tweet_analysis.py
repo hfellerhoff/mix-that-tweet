9010c844f1f5b1a02c66b1c2e194cfcacc224a97
Brian Nguyen <bp.nguyen05@gmail.com>
2021-12-09 21:42:01 -0600

    Added Azure sentiment analysis functionalities

--- flaskDemo/tweet_analysis.py
+++ flaskDemo/tweet_analysis.py
@@ -1,97 +1,75 @@
 import pandas as pd
 import requests
 import json
-import ast
 import yaml
 import datetime
 
-########## API Handling ##########
+# pip install azure-ai-textanalytics
+from azure.ai.textanalytics import TextAnalyticsClient
+from azure.core.credentials import AzureKeyCredential
 
-def create_twitter_url():
-    id = 'ids=1465762217480097794'
-    #tweet_fields = 'tweet.fields=created_at,public_metrics,non_public_metrics,possibly_sensitive'
-    tweet_fields = 'tweet.fields=id,created_at,public_metrics,possibly_sensitive'
-    expansions = 'expansions=author_id'
-    user_fields = 'user.fields=created_at,public_metrics'
-    url = 'https://api.twitter.com/2/tweets?{}&{}&{}&{}'.format(id, tweet_fields, expansions, user_fields)
-    return url
+########## API Keys and Tokens ##########
+
+# TWITTER_BEARER_TOKEN = os.environ['TWITTER_BEARER_TOKEN']
+# AZURE_SUBSCRIPTION_KEY = os.environ['AZURE_SUBSCRIPTION_KEY']
+# AZURE_ENDPOINT = os.environ['AZURE_ENDPOINT']
+
+########## API Handling ##########
 
 def process_yaml():
     with open('config.yaml') as file:
         return yaml.safe_load(file)
 
-def create_bearer_token(data):
-    return data['search_tweets_api']['bearer_token']
+def process_tweet_link(link):
+    link = link.split('/')
+    for index, component in enumerate(link):
+        if component == 'status':
+            id = link[index + 1]
+            if id.count('?') != 0:
+                return id[:id.index('?')]
+            else:
+                return id
+            
 
-def twitter_auth_and_connect(bearer_token, url):
+def create_twitter_url(tweet_id):
+    id = 'ids=' + str(tweet_id)
+    tweet_fields = 'tweet.fields=id,created_at,public_metrics,possibly_sensitive'
+    expansions = 'expansions=author_id'
+    user_fields = 'user.fields=created_at,public_metrics'
+    url = 'https://api.twitter.com/2/tweets?{}&{}&{}&{}'.format(
+        id, 
+        tweet_fields, 
+        expansions, 
+        user_fields
+    )
+    return url
+
+def twitter_auth_and_connect(yaml_data, url):
     headers = {
-        'Authorization': 'Bearer {}'.format(bearer_token),
+        # 'Authorization': 'Bearer {}'.format(TWITTER_BEARER_TOKEN), # ENV
+        'Authorization': 'Bearer {}'.format(yaml_data['twitter']['bearer_token']), # YAML
         'User-Agent': 'v2TweetLookupPython'
     }
     response = requests.request('GET', url, headers=headers)
     return response.json()
 
-def lang_data_shape(res_json):
-    data_only = res_json['data']
-    doc_start = '\'documents\': {}'.format(data_only)
-    str_json = '{' + doc_start + '}'
-    dump_doc = json.dumps(str_json)
-    doc = json.loads(dump_doc)
-    return ast.literal_eval(doc)
+def authenticate_azure_client(yaml_data):
+    # ta_credential = AzureKeyCredential(AZURE_SUBSCRIPTION_KEY) # ENV
+    ta_credential = AzureKeyCredential(yaml_data['azure']['subscription_key']) # YAML
+    text_analytics_client = TextAnalyticsClient(
+            # endpoint = AZURE_ENDPOINT, # ENV
+            endpoint = yaml_data['azure']['endpoint'], # YAML
+            credential = ta_credential)
+    return text_analytics_client
 
-def connect_to_azure(data):
-    azure_url = 'https://week.cognitiveservices.azure.com/'
-    language_api_url = '{}text/analytics/v2.1/languages'.format(azure_url)
-    sentiment_url = '{}text/analytics/v2.1/sentiment'.format(azure_url)
-    subscription_key = data['azure']['subscription_key']
-    return language_api_url, sentiment_url, subscription_key
-
-def azure_header(subscription_key):
-    return {'Ocp-Apim-Subscription-Key': subscription_key}
-
-def generate_languages(headers, language_api_url, documents):
-    response = requests.post(language_api_url, headers=headers, json=documents)
-    return response.json()
-
-
-def combine_lang_data(documents, with_languages):
-    langs = pd.DataFrame(with_languages['documents'])
-    lang_iso = [x.get('iso6391Name')
-                for d in langs.detectedLanguages if d for x in d]
-    data_only = documents['documents']
-    tweet_data = pd.DataFrame(data_only)
-    tweet_data.insert(2, 'language', lang_iso, True)
-    json_lines = tweet_data.to_json(orient='records')
-    return json_lines
-
-
-def add_document_format(json_lines):
-    docu_format = ''' + 'documents' + '''
-    json_docu_format = '{}:{}'.format(docu_format, json_lines)
-    docu_align = '{' + json_docu_format + '}'
-    jd_align = json.dumps(docu_align)
-    jl_align = json.loads(jd_align)
-    return ast.literal_eval(jl_align)
-
-
-def sentiment_scores(headers, sentiment_url, document_format):
-    response = requests.post(
-        sentiment_url, headers=headers, json=document_format)
-    return response.json()
-
-
-def mean_score(sentiments):
-    sentiment_df = pd.DataFrame(sentiments['documents'])
-    return sentiment_df['score'].mean()
-
-
-def week_logic(week_score):
-    if week_score > 0.75 or week_score == 0.75:
-        print('You had a positve week')
-    elif week_score > 0.45 or week_score == 0.45:
-        print('You had a neautral week')
-    else:
-        print('You had a negative week, I hope it gets better')
+def sentiment_analysis(client, text):
+    doc = [text]
+    response = client.analyze_sentiment(documents = doc)[0]
+    overall_sentiment = response.sentiment
+    positive_sentiment_score = response.confidence_scores.positive
+    neutral_sentiment_score = response.confidence_scores.neutral
+    negative_sentiment_score = response.confidence_scores.negative
+    return overall_sentiment, positive_sentiment_score, neutral_sentiment_score, negative_sentiment_score
 
 ########## Metric Conversions ##########
 
@@ -136,54 +114,90 @@ def pretty(doc):
 
 ########## Driver ##########
 
-def main():         
-    data = process_yaml()
-    bearer_token = create_bearer_token(data)
-    res_json = twitter_auth_and_connect(bearer_token, create_twitter_url())
-    print(pretty(res_json))
+def main():
+    yaml_data = process_yaml()
 
-    # documents = lang_data_shape(res_json)
-    # language_api_url, sentiment_url, subscription_key = connect_to_azure(data)
-    # headers = azure_header(subscription_key)
-    # with_languages = generate_languages(headers, language_api_url, documents)
-    # json_lines = combine_lang_data(documents, with_languages)
-    # document_format = add_document_format(json_lines)
-    # sentiments = sentiment_scores(headers, sentiment_url, document_format)
-    # week_score = mean_score(sentiments)
-    # print(week_score)
-    # week_logic(week_score)
+    testLink1 = "https://twitter.com/i/web/status/1465762217480097794?s=20"
+    testLink2 = "https://twitter.com/FarhangNamdar/status/1230554753182003203?ref_src=twsrc%5Etfw"
+    processed_tweet_id = process_tweet_link(testLink1)
+
+    tweet_obj = twitter_auth_and_connect(yaml_data, create_twitter_url(processed_tweet_id))
+    print('\nTweet lookup:\n', pretty(tweet_obj))
+
+    text = tweet_obj['data'][0]['text']
+    # Just commenting below out to save API requests
+    azure_client = authenticate_azure_client(yaml_data)
+    overall_sentiment, positive_sentiment_score, neutral_sentiment_score, negative_sentiment_score = sentiment_analysis(azure_client, text)
+
+    print()
+
+    print('Azure text sentiment analysis: {}'.format(overall_sentiment))
+    print('Sentiment scores: \n\tPositive={} \n\tNeutral={} \n\tNegative={}'.format(
+        positive_sentiment_score,
+        neutral_sentiment_score,
+        negative_sentiment_score,
+    ))
 
     print()
     
-    # Tweet object
-    tweet_id = 'tweet1' # Either DB-generated index or from 
-    tweet_url = create_twitter_url()
-    text = res_json['data'][0]['text']
-    user_handle = res_json['includes']['users'][0]['name']
-    created_at = res_json['data'][0]['created_at']
-    retweet_count = res_json['data'][0]['public_metrics']['retweet_count']
-    reply_count = res_json['data'][0]['public_metrics']['reply_count']
-    like_count = res_json['data'][0]['public_metrics']['like_count']
-    # quote_count = res_json['data'][0]['public_metrics']['quote_count']
-    impression_count = 353 # Need from 'non_public_metrics' w/ Elevated Twitter API authentication
-    url_link_clicks = 353 # Need from 'non_public_metrics' w/ Elevated Twitter API authentication
-    user_profile_clicks = 353 # Need from 'non_public_metrics' w/ Elevated Twitter API authentication
-    positive_sentiment_percentage = 0.33 # Need from Azure API
-    neutral_sentiment_percentage = 0.33 # Need from Azure API
-    negative_sentiment_percentage = 0.33 # Need from Azure API
+    # Tweet data
+    tweet_id = tweet_obj['data'][0]['id']
+    tweet_url = create_twitter_url(processed_tweet_id)
+    tweet_created_at = tweet_obj['data'][0]['created_at']
+    retweet_count = tweet_obj['data'][0]['public_metrics']['retweet_count']
+    reply_count = tweet_obj['data'][0]['public_metrics']['reply_count']
+    like_count = tweet_obj['data'][0]['public_metrics']['like_count']
+    quote_count = tweet_obj['data'][0]['public_metrics']['quote_count']
+
+    # User data
+    user_id = tweet_obj['includes']['users'][0]['id']
+    name = tweet_obj['includes']['users'][0]['name']
+    username = tweet_obj['includes']['users'][0]['username']
+    user_created_at = tweet_obj['includes']['users'][0]['created_at']
+    followers_count = tweet_obj['includes']['users'][0]['public_metrics']['followers_count']
+    following_count = tweet_obj['includes']['users'][0]['public_metrics']['following_count']
+    tweet_count = tweet_obj['includes']['users'][0]['public_metrics']['tweet_count']
+    listed_count = tweet_obj['includes']['users'][0]['public_metrics']['listed_count']
 
     # Tweet SQL insertion DDL
-    insert_ddl = 'INSERT INTO Tweet(tweet_id,tweet_url,user_handle,created_at,impression_count,like_count,reply_count,retweet_count,url_link_clicks,user_profile_clicks,positive_sentiment_percentage,neutral_sentiment_percentage,negative_sentiment_percentage) VALUES (\'{}\',\'{}\',\'{}\',\'{}\',{},{},{},{},{},{});'.format(tweet_id,tweet_url,user_handle,created_at,impression_count,like_count,reply_count,retweet_count,url_link_clicks,user_profile_clicks,positive_sentiment_percentage,neutral_sentiment_percentage,negative_sentiment_percentage)
-    print('Tweet SQL insertion DDL:\n', get_ddl(insert_ddl))
+    tweet_insert_ddl = 'INSERT INTO Tweet(tweet_id,tweet_url,tweet_created_at,text,retweet_count,reply_count,like_count,quote_count,positive_sentiment_percentage,neutral_sentiment_percentage,negative_sentiment_percentage) VALUES ("{}","{}","{}","{}",{},{},{},{},{},{},{});'.format(
+        tweet_id,
+        tweet_url,
+        tweet_created_at,
+        text,
+        retweet_count,
+        reply_count,
+        like_count,
+        quote_count,
+        positive_sentiment_score,
+        neutral_sentiment_score,
+        negative_sentiment_score,
+    )
+    print('Tweet SQL insertion DDL:\n', get_ddl(tweet_insert_ddl))
+
+    print()
+
+    # User SQL insertion DDL
+    user_insert_ddl = 'INSERT INTO User(user_id,name,username,user_created_at,followers_count,following_count,tweet_count,listed_count) VALUES ("{}","{}","{}","{}",{},{},{},{});'.format(
+        user_id,
+        name,
+        username,
+        user_created_at,
+        followers_count,
+        following_count,
+        tweet_count,
+        listed_count,
+    )
+    print('User SQL insertion DDL:\n', get_ddl(user_insert_ddl))
 
     print()
 
     # Tweet object to Spotify audio features
-    time_period = (datetime.date.today() - datetime.date(int(created_at[0:4]), int(created_at[5:7]), int(created_at[8:10]))).days # In days
+    time_period = (datetime.date.today() - datetime.date(int(tweet_created_at[0:4]), int(tweet_created_at[5:7]), int(tweet_created_at[8:10]))).days # In days
     danceability = retweet_count
-    valence = positive_sentiment_percentage - negative_sentiment_percentage
+    valence = positive_sentiment_score - negative_sentiment_score
     energy = reply_count
-    tempo = positive_sentiment_percentage + negative_sentiment_percentage
+    tempo = positive_sentiment_score + negative_sentiment_score
     loudness = calculate_loudness(text)
     speechiness = calculate_speechiness(text)
     instrumentalness = like_count / reply_count
@@ -204,6 +218,8 @@ def main():
         'acousticness': acousticness
     }
     print('Tweet to Audio Features mapping:\n', get_mapping(tweet_to_audiofeatures_map))
+
+    print()
 
     # Perhaps sort subset of matching songs by popularity and choose most popular songs for virability?
 
