477abd4ad7a376086b3455682146328a3390e01e
Sophie Von Hatten <svonhatten@luc.edu>
2021-12-10 18:38:50 -0600

    get tweeter to add to the db

--- flaskDemo/forms.py
+++ flaskDemo/forms.py
@@ -2,13 +2,19 @@ from flask_wtf import FlaskForm
 from flask_wtf.file import FileField, FileAllowed
 from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField, IntegerField, DateField, SelectField, HiddenField
-from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Regexp
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError,Regexp, URL
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
 from flaskDemo import db
 from flaskDemo.models import Include, Playlist, Song, Tweet
 from wtforms.fields.html5 import DateField
 
 class TweetForm(FlaskForm):
-    tweet_url=StringField('Tweet URL', validators=[DataRequired()])
+    tweet_url=StringField('Tweet URL', validators=[DataRequired(), URL()])
     submit=SubmitField('Submit')
+
+    # TODO: validation for tweet
+    # def validate_tweet(self, tweet_url):
+        # tweet_url = Tweet.query.filter_by(tweet_url=tweet_url.data).first()
+
+        # if tweet_url:
 
--- flaskDemo/routes.py
+++ flaskDemo/routes.py
@@ -4,15 +4,32 @@ from PIL import Image
 from flask import render_template, url_for, flash, redirect, request, abort
 from flaskDemo import app, db, bcrypt
 from flaskDemo.forms import TweetForm
-from flaskDemo.models import Include, Playlist, Song, Tweet
+from flaskDemo.models import Include, Playlist, Song, Tweet, Tweeter
 from flask_login import login_user, current_user, logout_user, login_required
 from datetime import datetime
+from flaskDemo.tweet_analysis import analyzeTweet
 
 
 @app.route("/")
 @app.route("/home")
 def home():
     form = TweetForm()
+    if form.validate_on_submit():
+        tweet_dict, tweeter_dict, tweet_to_audiofeatures_map = analyzeTweet(form.tweet_url.data)
+        tweeter = Tweeter(
+            tweeter_id=tweeter_dict["tweeter_id"],
+            tweeter_name=tweeter_dict["tweeter_name"],
+            tweeter_username=tweeter_dict["tweeter_username"],
+            tweeter_created_at=tweeter_dict["tweeter_created_at"],
+            followers_count=tweeter_dict["followers_count"],
+            following_count=tweeter_dict["following_count"],
+            tweet_count=tweeter_dict["tweet_count"],
+            listed_count=tweeter_dict["listed_count"]
+        )
+        db.session.add(tweeter)
+        db.session.commit()
+        flash('Your tweet has been added! You are now able to see your playlist', 'success')
+        return redirect(url_for('playlist/<playlist_id>'))
     return render_template('home.html', form=form)
 
    
